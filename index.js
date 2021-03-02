0ab76dbe23c7c1480bfec8b6d6ef84f3d00dcdd4
balanced-checkpoint-e039 <harmandeepk628@gmail.com>
2021-03-02 20:52:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ function sayHiToGrandma(string){
     return "I can't hear you!"
   }
 
-  if (string.toUpperCase()){
-    return "YES INDEED!"
+  else if (string.toUpperCase()){
+    return "yes indeed!"
   }
 
-  if (string === "I love you, Grandma."){
+  else if (string === "I love you, Grandma."){
     return "I love you, too."
   }
 
